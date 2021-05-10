47ee680989da607ebcfa228868deae62c3fc6f87
Axel Figueredo <axel@beltsistemas.com.br>
2021-05-09 21:23:14 -0300

    Aula DIO - JPA Basico

---
+++ README.md
@@ -0,0 +1,23 @@
+# Projeto
+Aula básica sobre JPA disponibilizada na Digital Innovation One (https://digitalinnovation.one)
+
+## Passos iniciais
+As seguintes instruções irão criar uma cópia do projeto na sua máquina local para realizar os testes executados em aula.
+
+### Pré requisitos usados em aula
+- Java Development Kit (JDK) – 1.8 ou superior
+- IntelliJ IDEA (É recomendável seguir com essa IDE, porém pode ser selecionada outra)
+- Gradle 5.3.1 (É utilizado para baixar o Driver JDBC. Se quiser, pode ser instalado manualmente ou com o Maven)
+- MySQL (Passos para instalação estão no arquivo "DatabaseInstructions" (https://github.com/afigueredo-stack/jdbc-basic/tree/master/src/main/resources/DatabaseInstructions.txt)
+
+### Instalação
+Com os pré requisitos instalados, basta baixar esse projeto no seu IDE (através do comando "git clone") e seguir os passos que estão no arquivo "DatabaseInstructions" (https://github.com/afigueredo-stack/jdbc-basic/tree/master/src/main/java/resources/DatabaseInstructions.txt) para instanciar os bancos de dados, tabelas e alguns registros para teste.
+
+## Executar
+Basta rodar a função "main" da classe compilada de uma das seguintes classes:
+
+- JpaComImplementacao.java (https://github.com/afigueredo-stack/jpa-basic/tree/master/src/main/java/JpaComImplementacao.java)
+
+- JpaSemImplementacao.java (https://github.com/afigueredo-stack/jpa-basic/tree/master/src/main/java/JpaSemImplementacao.java)
+
+- JpqlImplementado.java (https://github.com/afigueredo-stack/jpa-basic/tree/master/src/main/java/JpqlImplementado.java)
---
+++ src/main/java/JpaComImplementacao.java
@@ -0,0 +1,61 @@
+import entity.Aluno;
+import entity.Estado;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaComImplementacao {
+
+    public static void main(String[] args) {
+
+        // OBS: Esse codigo deve executar SEM ERROS com a implementacao JPA que foi definida no "persistence.xml".
+        // Se estiver somente com o JPA baixado, o codigo NAO IRA funcionar.
+
+        // O ideal é que nessa parte (Parte 2) o codigo EXECUTE SEM ERROS, pois aqui tera uma implementacao JPA sendo utilizada.
+
+        // 1 - Passos iniciais para criar um gerenciador de entidades com o banco de dados especificado no arquivo  "persistence.xml"
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("part2-DIO");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        // 2.1 - Criar instancias para serem adicionadas no banco de dados
+        Estado estadoParaAdicionar = new Estado("Rio Grande do Norte", "RN");
+        Aluno alunoParaAdicionar = new Aluno("Neguinha", 30, estadoParaAdicionar);
+
+        // 2.2 - Iniciar uma transacao para adicionar as instancias no banco de dados
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(estadoParaAdicionar);
+        entityManager.persist(alunoParaAdicionar);
+
+        entityManager.getTransaction().commit();
+
+        // 3 - Resgatar instâncias no banco de dados
+        Estado estadoEncontrado = entityManager.find(Estado.class, 1);
+        Aluno alunoEncontrado = entityManager.find(Aluno.class, 1);
+
+        System.out.println(estadoEncontrado);
+        System.out.println(alunoEncontrado);
+
+        // 4 - Alterar uma entidade
+        entityManager.getTransaction().begin();
+
+        alunoEncontrado.setNome("Axel");
+        alunoEncontrado.setIdade(27);
+
+        entityManager.getTransaction().commit();
+
+        // 5 - Remover uma entidade
+        entityManager.getTransaction().begin();
+
+        entityManager.remove(alunoEncontrado);
+
+        entityManager.getTransaction().commit();
+
+        // 6 - Encerrar o gerenciador de entidades e encerrar a fabrica de gerenciadores de entidade.
+        entityManager.close();
+        entityManagerFactory.close();
+
+    }
+
+}
